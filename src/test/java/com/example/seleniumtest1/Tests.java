142ccac07e7e6eb4ffcc400546df512a0b27a0b4
Rebecca <rebfje-1@student.ltu.se>
2022-05-04 21:37:19 +0200

    updated wait time between methods

--- src/test/java/com/example/seleniumtest1/Tests.java
+++ src/test/java/com/example/seleniumtest1/Tests.java
@@ -7,6 +7,8 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class Tests {
 
     @BeforeSuite
@@ -16,13 +18,15 @@ public class Tests {
 
 
 
-    @Test(testName = "Survey tests", invocationCount = 3)
-    public static void submitSurvey() {
+    @Test(testName = "Survey tests")
+    public static void submitSurvey() throws InterruptedException {
         System.setProperty("webdriver.edge.driver","C:\\Users\\Rebecca\\Documents\\Edgedriver\\msedgedriver.exe");
         EdgeDriver driver = new EdgeDriver();
         driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdpAvMHvw1LKPX8ZD-zICG7yB3MMb57d6flvlYv2ptiJ5Mg9A/viewform?usp=sf_link");
         Survey survey = new Survey(driver);
+        Thread.sleep(2000);
         survey.chooseOption();
+        Thread.sleep(2000);
         survey.submitSurvey();
     }
 
